c9509847fdc950d8d6fec2d642392f675b05c710
Rodolf <rodolfo.brah@gmail.com>
2022-02-13 17:50:51 -0300

    let

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import TimeRodolf from "./components/RodolfTime";
+import Timer from "./components/RodolfDesafil/RodolfDesafil";
+
+function App() {
+    
+
+    return (
+        <>
+            <Timer></Timer>
+           
+        </>
+    );
+}
+
+export default App;
