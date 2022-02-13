88664ba4f1cb2531ab2ff2f052e4435c7113242f
josuelJFS <josuelafk@gmail.com>
2022-02-13 13:40:33 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,11 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-    const [cont, setCont] = useState<number>(0);
-    const [next, setNext] = useState<number>(0);
-
-    function increment() {
-        setCont(cont + 1);
-    }
-
-    function nextF() {
-        setNext(next + 1);
-    }
-
-    useEffect(() => {
-        // toda vez que cont mudar o useEffect reexeculta
-
-        console.log("pagina api");
-        
-    }, [cont]);
-
-    useEffect(()=>{
-      console.log("next"+next);
-    },[next])
+    
 
     return (
         <>
-            <h1 className="App">{cont}</h1>
-            <button onClick={increment}>pagina</button>
-            <button onClick={nextF}>next</button>
+           
         </>
     );
 }
