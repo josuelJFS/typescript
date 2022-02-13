ca3e9f6fdfe5d1cddb27709c53d5678f73008d24
josuelJFS <josuelafk@gmail.com>
2022-02-13 17:15:22 -0300

    master

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import TimeJosuel from "./components/josuelTime";
+import RefExemple from "./components/usandoref/exemplo";
 
 function App() {
     
 
     return (
         <>
-           
+           <TimeJosuel />
+           <RefExemple />
         </>
     );
 }
